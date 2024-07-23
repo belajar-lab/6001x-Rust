4ebea6e9afb55f0741fe72b47da713d8ea5b540d
Belajar <bljkmp@gmail.com>
2024-07-23 23:52:55 +0000

    Working problem 1

--- problem_set_1/src/main.rs
+++ problem_set_1/src/main.rs
@@ -1,3 +1,14 @@
 fn main() {
-    println!("Hello, world!");
+    let s = "azcbobobegghakl";
+
+    let vowels = ["a", "e", "i", "o", "u"];
+    let mut vowels_count = 0;
+
+    for c in s.chars() {
+        if vowels.contains(c.to_lowercase().next().unwrap()) {
+            vowels_count += 1;
+        }
+    }
+
+    println!("Number of vowels: {}", vowels_count);
 }
