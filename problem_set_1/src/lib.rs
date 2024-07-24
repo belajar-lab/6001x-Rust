2ac42bb3e02ed9badfe281db52e50fba0cd752e8
Belajar <80247425+belajar-lab@users.noreply.github.com>
2024-07-24 02:02:40 +0000

    Add test case for Problem 1

--- problem_set_1/src/lib.rs
+++ problem_set_1/src/lib.rs
@@ -49,10 +49,29 @@ pub fn longest_substring(s: &str) -> String {
 
 #[cfg(test)]
 mod tests {
-    // use super::*;
+    use super::*;
 
     #[test]
     fn problem_1_tests() {
-        // assert_eq!(result, 4);
+        assert_eq!(count_vowels("hetseoxbadk"), 4);
+        assert_eq!(count_vowels("numejfiqjajlfdihyhfzuoo"), 8);
+        assert_eq!(count_vowels("owgseupxbp"), 3);
+        assert_eq!(count_vowels("kuvabhxwhaqhspsddakpwow"), 5);
+        assert_eq!(count_vowels("agurzqshtcbxnm"), 2);
+        assert_eq!(count_vowels("hyueqpacbbohn"), 4);
+        assert_eq!(count_vowels("ytaaubrvxjaoq"), 5);
+        assert_eq!(count_vowels("youoeoiesjunfuxyocmmqx"), 10);
+        assert_eq!(count_vowels("dybmaa"), 2);
+        assert_eq!(count_vowels("rlaadtsynfuoroqxxegh"), 6);
+        assert_eq!(count_vowels("payideo"), 4);
+        assert_eq!(count_vowels("nnyaaasmthqeygill"), 5);
+        assert_eq!(count_vowels("thaauqufhrz"), 4);
+        assert_eq!(count_vowels("lvousenubil"), 5);
+        assert_eq!(count_vowels("vnckialhxeumsenqdiw"), 6);
+        assert_eq!(count_vowels("gbmhhezwctmhzaihw"), 3);
+        assert_eq!(count_vowels("wacqagqfbdqgeuytu"), 5);
+        assert_eq!(count_vowels("xsiedrejauowtql"), 6);
+        assert_eq!(count_vowels("cozftregup"), 3);
+        assert_eq!(count_vowels("otqitwoccqlvuio"), 6);
     }
 }
