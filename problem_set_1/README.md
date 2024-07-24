e084a70ba4d57750fc7cd1626ad62ff45e75229c
Belajar <80247425+belajar-lab@users.noreply.github.com>
2024-07-24 01:06:28 +0000

    Problem 2 completed

--- problem_set_1/README.md
+++ problem_set_1/README.md
@@ -7,3 +7,13 @@ Write a program that counts up the number of vowels contained in the string `s`
 ```
 Number of vowels: 5
 ```
+
+## Problem 2
+
+Assume `s` is a string of lower case characters.
+
+Write a program that prints the number of times the string `'bob'` occurs in `s`. For example, if `s = 'azcbobobegghakl'`, then your program should print
+
+```
+Number of times bob occurs is: 2
+```
--- problem_set_1/src/main.rs
+++ problem_set_1/src/main.rs
@@ -9,6 +9,25 @@ fn problem_1() {
     println!("Number of vowels: {}", vowels_count);
 }
 
+fn problem_2() {
+    let s = "azcbobobegghakl";
+
+    let chars: Vec<char> = s.chars().collect();
+    let mut counter = 0;
+    let mut start = 0;
+    let mut end = 3;
+
+    while end <= chars.len() {
+        if chars[start..end] == ['b', 'o', 'b'] {
+            counter += 1;
+        }
+        start += 1;
+        end += 1;
+    }
+
+    println!("Number of times bob occurs is: {}", counter);
+}
+
 fn main() {
-    problem_1();
+    problem_2();
 }
